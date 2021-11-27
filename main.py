53828555e47d73778b71f62a9c3d529d3b263b25
endorgobio <endorgobio@gmail.com>
2021-11-26 21:04:58 -0500

    Consider additional features working. No pipeline

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import scipy
 import spacy
 from spacy.lang.es import Spanish
 from sklearn.compose import ColumnTransformer 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -83,9 +83,7 @@ dfPQR['Riesgo_vida_encode'] = [1 if x =='SI' else 0 for x in dfPQR['Riesgo_vida'
 #stop_words.extend(("amp", "xa", "xe"))
 
 
-
-
-
+# NLP 
 nlp = Spanish()
 nlp = spacy.load("es_core_news_sm")
 stop_words = spacy.lang.es.stop_words.STOP_WORDS
@@ -206,7 +204,7 @@ print("Logistic Regression Recall:",metrics.recall_score(y_test, predicted))
 #####################################################
 
 # To run with less data in debug (faster)
-dfPQR = dfPQR.sample(1000)
+# dfPQR = dfPQR.sample(1000)
 # Get data 
 X_data = dfPQR[['Contenido', 'Edad']]
 Y_data = dfPQR['Riesgo_vida_encode']
@@ -232,17 +230,62 @@ tfidf_train = tfidf_vectorizador.transform(X_train['Contenido'])
 tfidf_test  = tfidf_vectorizador.transform(X_test['Contenido'])
 
 numerical_trasnsf = ColumnTransformer(
-    [('scaler', StandardScaler(), ['Edad'])],
+    [('scaler', MinMaxScaler(), ['Edad'])],
     remainder='passthrough') 
 edad_scaled_train = numerical_trasnsf.fit_transform(X_train['Edad'].to_frame())
 edad_scaled_test = numerical_trasnsf.fit_transform(X_test['Edad'].to_frame())
-# AQUI VOY
+
+
+tfidf_train_plus = scipy.sparse.hstack([tfidf_train, edad_scaled_train])
+tfidf_test_plus = scipy.sparse.hstack([tfidf_test, edad_scaled_test])
+
 # Fit the classifier
-classifier.fit(X=tfidf_train, y= y_train)
+classifier.fit(X=tfidf_train_plus, y= y_train)
 
 # Get predictions
 
-predicted = classifier.predict(X=tfidf_test)
+predicted = classifier.predict(X=tfidf_test_plus)
+
+# Model Accuracy
+print("Logistic Regression Accuracy:",metrics.accuracy_score(y_test, predicted))
+print("Logistic Regression Precision:",metrics.precision_score(y_test, predicted))
+print("Logistic Regression Recall:",metrics.recall_score(y_test, predicted))
+
+
+# Save the models
+# create an iterator object with write permission - model.pkl
+with open('vectorizer_extraFeatures.pkl', 'wb') as files:
+    pickle.dump(tfidf_vectorizador, files)
+
+# create an iterator object with write permission - model.pkl
+with open('lg_extraFeatures.pkl', 'wb') as files:
+    pickle.dump(classifier, files)
+
+
+    
+    
+# Test models
+# load saved model
+with open('vectorizer_extraFeatures.pkl' , 'rb') as f:
+    vectorizer_load = pickle.load(f)
+    
+with open('lg_extraFeatures.pkl' , 'rb') as f:
+    classifier_load = pickle.load(f)
+  
+
+tfidf_test2  = vectorizer_load.transform(X_test['Contenido'])
+edad_scaled_test = numerical_trasnsf.fit_transform(X_test['Edad'].to_frame())
+tfidf_test_plus = scipy.sparse.hstack([tfidf_test, edad_scaled_test])
+predicted = classifier.predict(X=tfidf_test_plus)
+
+print("Logistic Regression Accuracy:",metrics.accuracy_score(y_test, predicted))
+print("Logistic Regression Precision:",metrics.precision_score(y_test, predicted))
+print("Logistic Regression Recall:",metrics.recall_score(y_test, predicted))
+
+#####################################################
+# 4. with contenido and extra columns as predictors using pipeline 
+#####################################################
+
 
 column_trans = ColumnTransformer(
     [('scaler', StandardScaler(), ["Edad"])],
@@ -329,27 +372,6 @@ tfidf_test  = tfidf_vectorizador.transform(X_test)
 
 
 
-column_trans = ColumnTransformer(
-    [('scaler', StandardScaler(), ["Edad"])],
-    remainder='passthrough') 
-data = column_trans.fit_transform(X_train)
-
-
-# Add columns
-
-new_feature  =  data[:,0]
-tfidf_train_plus = scipy.sparse.hstack([tfidf_train, new_feature])
-new_feature = dfPQR[['Edad']].sample(tfidf_test.shape[0])
-new_feature['Edad']  = np.random.rand()
-tfidf_test_plus = scipy.sparse.hstack([tfidf_test, new_feature])
-
-
-weights = {0:1, 1:2.5}
-classifier = LogisticRegression(class_weight=weights)
-
-classifier.fit(X=tfidf_train, y= y_train)
-
-predicted = classifier.predict(X=tfidf_test_plus)
 
 
 # Model Accuracy
@@ -357,7 +379,9 @@ print("Logistic Regression Accuracy:",metrics.accuracy_score(y_test, predicted))
 print("Logistic Regression Precision:",metrics.precision_score(y_test, predicted))
 print("Logistic Regression Recall:",metrics.recall_score(y_test, predicted))
 
-# Print performance metrics
+#####################################################
+# 5. Print performance metrics 
+#####################################################
 
 
 confusion_matrix(y_true = y_test, y_pred= predicted)
@@ -372,8 +396,10 @@ result_grouped = result.groupby(['Riesgo_vida_textual', 'y_real', 'y_hat']).y_ha
     suma=('count')
 )
   
-print(result)
 
+#####################################################
+# 6. Save models using pickle 
+#####################################################
 
 # save the model to disk
 filenameVect = 'vectorizerPQRs.sav'
@@ -381,7 +407,7 @@ pickle.dump(pipe['vectorizer'], open(filenameVect, 'wb'))
 filenameLR = 'logRegPQRs.sav'
 pickle.dump(pipe['classifier'], open(filenameLR, 'wb'))
 
-# Cargue los modelos previamente entrenados
+# Load models from pickle
 vectorizer_url = 'https://docs.google.com/uc?export=download&id=1QOVxd0R7UctnUpwlDKldHgUhIPFSt32M'
 vectorizerPQRs = wget.download(vectorizer_url, 'vectorizerPQRs.sav')
 lr_url = 'https://docs.google.com/uc?export=download&id=1y-pDeJCWM413aijsux4iMVP_bjRcE73X'
@@ -392,10 +418,6 @@ loaded_logReg = pickle.load(open(lrPQRs, 'rb'))
 
 
 
-# Predicting with a test dataset
-# Create pipeline using Bag of Words
-pipe = Pipeline([('vectorizer', loaded_vectorizer),
-                 ('classifier', loaded_logReg)])
 
 
 
